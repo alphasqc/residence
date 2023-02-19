1c72d149a013df64e9108645945ac25202b91bb6
alphasqc <sqc19990726@163.com>
2023-02-19 19:28:43 +0800

    addcontent

---
+++ src/views/primary/PrimaryView.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="primary">
+        <nav class="navbox">
+            <a href="">综合</a>
+            <a href="">关注</a>
+            <a href="">前端</a>
+            <a href="">后端</a>
+            <a href="">测试</a>
+            <a href="">UI</a>
+        </nav>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+@import '@style/primary.css';
+</style>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,59 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  // 设置路径别名
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+      '@components': resolve(__dirname, './src/components'),
+      '@style': resolve(__dirname, './src/style'),
+      '@views': resolve(__dirname, './src/views'),
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "./src/style/index.css";'
+      },
+    },
+  },
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
+  server: {
+    host: true,
+    // 代理端口
+    port: 3000,
+    // 自动打开浏览器
+    // open: true,
+    proxy: {
+      "/api": {
+        // 目标地址
+        target: "http://127.0.0.1:3001",
+        // 允许跨域
+        changeOrigin: true,
+        // 开启websockets代理
+        ws: true,
+        // 验证ssl证书
+        // secure: false,
+        // pathRewrite: {
+        //   '^/api': '/', // rewrite path
+        // },
+        // 重写path路径
+        rewrite: (path) => path.replace(/^\/api/, "")
+      }
+    }
+  }
 })
